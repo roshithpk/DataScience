90165951c5cfa622e3c7aefc78c94364acb33bc9
roshithpk <166591536+roshithpk@users.noreply.github.com>
2025-07-28 12:35:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import streamlit as st
+from stock import stock_page
+
+st.title("Stock & Gold Price Data Fetcher")
+
+menu = st.selectbox("Select Data Type", ["Stock"])  # For now only Stock
+
+if menu == "Stock":
+    stock_page()
 
